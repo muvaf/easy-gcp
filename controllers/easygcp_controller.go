0c04a903fddec44cf95cb841b5a1f4dfa05a8f9f
Muvaffak Onus <onus.muvaffak@gmail.com>
2019-12-05 18:45:42 +0300

    Continuously applying the resources
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

--- controllers/easygcp_controller.go
+++ controllers/easygcp_controller.go
@@ -18,6 +18,17 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	types2 "k8s.io/apimachinery/pkg/types"
+
+	"sigs.k8s.io/kustomize/api/types"
+
+	"github.com/crossplaneio/easy-gcp/pkg/operations"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -25,6 +36,11 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	gcpv1alpha1 "github.com/crossplaneio/easy-gcp/api/v1alpha1"
+)
+
+const (
+	shortWait = 30 * time.Second
+	longWait  = 1 * time.Minute
 )
 
 // EasyGCPReconciler reconciles a EasyGCP object
@@ -38,12 +54,52 @@ type EasyGCPReconciler struct {
 // +kubebuilder:rbac:groups=gcp.easystacks.crossplane.io,resources=easygcps/status,verbs=get;update;patch
 
 func (r *EasyGCPReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("easygcp", req.NamespacedName)
+	ctx := context.Background()
 
-	// your logic here
+	cr := &gcpv1alpha1.EasyGCP{}
+	if err := r.Client.Get(ctx, req.NamespacedName, cr); err != nil {
+		return ctrl.Result{RequeueAfter: shortWait}, err
+	}
+	// todo: use WasDeleted
+	if cr.DeletionTimestamp != nil {
+		return ctrl.Result{Requeue: false}, nil
+	}
+	err := operations.ProcessKustomization(func(k *types.Kustomization) {
+		k.NamePrefix = fmt.Sprintf("%s-", cr.GetName())
+		k.CommonLabels = map[string]string{
+			"gcp.configurationstacks.crossplane.io/name": cr.GetName(),
+			"gcp.configurationstacks.crossplane.io/uid":  string(cr.GetUID()),
+		}
+	})
+	objects, err := operations.RunKustomize()
+	for _, o := range objects {
+		if err := ApplyObject(ctx, r.Client, o); err != nil {
+			return ctrl.Result{RequeueAfter: shortWait}, err
+		}
+		fmt.Printf("deployed %s type!", o.GetObjectKind().GroupVersionKind().String())
+	}
+	return ctrl.Result{RequeueAfter: longWait}, err
+}
 
-	return ctrl.Result{}, nil
+func ApplyObject(ctx context.Context, kube client.Client, o runtime.Object) error {
+	type Object interface {
+		runtime.Object
+		v1.Object
+	}
+	resource, ok := o.(Object)
+	if !ok {
+		return fmt.Errorf("given object does not have metadata")
+	}
+	existing := resource.DeepCopyObject().(Object)
+	err := kube.Get(ctx, types2.NamespacedName{Name: resource.GetName(), Namespace: resource.GetNamespace()}, existing)
+	if errors.IsNotFound(err) {
+		return kube.Create(ctx, resource)
+	}
+	if err != nil {
+		return err
+	}
+	resource.SetResourceVersion(existing.GetResourceVersion())
+	return kube.Patch(ctx, resource, client.MergeFrom(existing))
 }
 
 func (r *EasyGCPReconciler) SetupWithManager(mgr ctrl.Manager) error {
--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/crossplaneio/easy-gcp
 go 1.12
 
 require (
+	github.com/go-logfmt/logfmt v0.4.0 // indirect
 	github.com/go-logr/logr v0.1.0
-	github.com/onsi/ginkgo v1.6.0
-	github.com/onsi/gomega v1.4.2
-	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+	github.com/onsi/ginkgo v1.10.1
+	github.com/onsi/gomega v1.7.0
+	gopkg.in/yaml.v2 v2.2.4
 	k8s.io/apimachinery v0.0.0-20190817020851-f2f3a405f61d
 	k8s.io/client-go v0.0.0-20190918200256-06eb1244587a
 	sigs.k8s.io/controller-runtime v0.3.0
+	sigs.k8s.io/kustomize v2.0.3+incompatible // indirect
+	sigs.k8s.io/kustomize/api v0.2.0
 )
